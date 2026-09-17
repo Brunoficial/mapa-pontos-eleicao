f921222a77500a2aa1d59037a502c7c84d84ad25
Bruno <bruno.lopes.vrls@gmail.com>
2026-09-17 10:32:54 -0300

    Create localDeVotacaoService.js

---
+++ backend/local_de_votacao/localDeVotacaoService.js
@@ -0,0 +1,49 @@
+import LocalDeVotacaoRepository from "../repositories/LocalDeVotacaoRepository.js";
+
+class LocalDeVotacaoService {
+    constructor(db) {
+        this.LocalDeVotacaoRepository = new LocalDeVotacaoRepository(db);
+    }
+
+    async listar(req, res) {
+        const locais = await this.LocalDeVotacaoRepository.listar();
+
+        res.status(200).json(locais);
+    }
+
+    async encontrarPorId(req, res) {
+        const { id } = req.params;
+
+        const local = await this.LocalDeVotacaoRepository.encontrarPorId(id);
+
+        if (!local) {
+            return res.status(404).json({
+                error: "Local de votação não encontrado"
+            });
+        }
+
+        return res.status(200).json(local);
+    }
+
+    async atualizar(req, res) {
+        const { id } = req.params;
+        const localAtualizado = req.body;
+
+        const local = await this.LocalDeVotacaoRepository.encontrarPorId(id);
+
+        if (!local) {
+            return res.status(404).json({
+                error: "Local de votação não encontrado"
+            });
+        }
+
+        await this.LocalDeVotacaoRepository.atualizar(
+            id,
+            localAtualizado
+        );
+
+        return res.status(204).send();
+    }
+}
+
+export default LocalDeVotacaoService;
