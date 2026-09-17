7845c4b757b8cb78ef23e6d0e031df82b0f746c7
Bruno <bruno.lopes.vrls@gmail.com>
2026-09-17 10:41:48 -0300

    Create localDeVotacaoRouter.js

---
+++ backend/local_de_votacao/localDeVotacaoRouter.js
@@ -0,0 +1,24 @@
+import LocalDeVotacaoService from "../services/LocalDeVotacaoService.js";
+import express from "express";
+import dbConnection from "../config/db.js";
+
+const localDeVotacaoRouter = express.Router();
+
+const localDeVotacaoService = new LocalDeVotacaoService(dbConnection);
+
+localDeVotacaoRouter.get(
+    "/listar",
+    async (req, res) => localDeVotacaoService.listar(req, res)
+);
+
+localDeVotacaoRouter.get(
+    "/encontrar/:id",
+    async (req, res) => localDeVotacaoService.encontrarPorId(req, res)
+);
+
+localDeVotacaoRouter.put(
+    "/atualizar/:id",
+    async (req, res) => localDeVotacaoService.atualizar(req, res)
+);
+
+export default localDeVotacaoRouter;
