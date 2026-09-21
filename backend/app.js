409e8ccfafc024e490b35f12e23ec22045c2cfb8
Bruno <bruno.lopes.vrls@gmail.com>
2026-09-21 08:06:49 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,23 @@
+import express from 'express'
+import localDeVotacaoRouter from './local_de_votacao/localDeVotacaoRouter.js'
+import cors from 'cors'
+import 'dotenv/config'
 
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const port = process.env.PORT || 3000 
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
+app.listen(port, () => {
+  console.log(`API rodando na porta ${port}`)
+})
+
+app.use('/local', localDeVotacaoRouter)
+
+
+export default app 
